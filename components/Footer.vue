260d68e00957f6ba712ff2158674e0d7ad713d58
Reuben Ninan <intelphysic@gmail.com>
2020-05-22 15:30:22 -0400

    footer and background

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="h-auto flex font-semibold text-sm bg-yellow-500 md:px-12 p-3">
+        <div class="w-1/2 text-left">
+            <p>Made with ❤️ by Reuben</p>
+        </div>
+        <div class=" w-1/2 text-right">
+            <p>2020. <b>Lv19 Production</b></p>
+        </div>
+    </div>
+</template>
---
+++ components/Splash.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bgimg min-h-screen bg-fixed bg-cover bg-center">
+
+    </div>
+
+</template>
+
+<style type="text/css">
+
+@import url('https://fonts.googleapis.com/css?family=Roboto|Nunito');
+* {
+    font-family: 'Nunito', sans-serif;
+  }
+.name{
+    font-family: 'Roboto', sans-serif;
+}
+.bgimg {
+    background-image: url('../assets/img/city.jpg');
+}
+.glass{
+    opacity: .99;
+}
+
+</style>
