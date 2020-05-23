cff9860cc0c3e74fd6c4a3ea0315905bf4bdbf57
Reuben Mathew <intelphysic@gmail.com>
2020-05-22 21:21:20 -0400

    added icons

--- components/NameCard.vue
+++ components/NameCard.vue
@@ -3,18 +3,27 @@
   <div class="flex max-w-md shadow-lg bg-gray-900 rounded-lg overflow-hidden">
      <div class="w-full p-6 text-white">
         <p class="font-bold mt-4 rounded-full text-base uppercase">Toronto, Ontario</p>
-        <p class="text-4xl md:text-5xl name font-semibold text-yellow-500">Reuben Ninan</p>
+        <p class="text-4xl md:text-5xl font-semibold text-yellow-500">Reuben Ninan</p>
         <p class="text-2xl mt-2 mb-6 leading-none font-light">Software Engineer</p>
 
         <div class=" text-gray-800 text-lg font-bold flex flex-wrap">
             <div class="py-2 pr-3">
-                <a href="https://github.com/ReubenMathew/" class="bg-yellow-500 transition duration-200 ease-in-out hover:text-yellow-500 border border-yellow-500 hover:bg-transparent px-4 py-1 shadow-lg rounded-full">Github</a>
+                <a href="https://github.com/ReubenMathew/" class="flex items-center bg-yellow-500 transition duration-200 ease-in-out hover:text-yellow-500 border border-yellow-500 hover:bg-transparent px-4 py-1 shadow-lg rounded-full">
+                    <svg class="h-5 w-5 mr-2 my-auto fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M32 .8C14.4.8 0 15.1 0 32.8c0 14.1 9.2 26.1 21.9 30.4 1.6.3 2.2-.7 2.2-1.5 0-.7 0-2.8-.1-5.5-8.9 2-10.8-4.3-10.8-4.3-1.5-3.7-3.6-4.6-3.6-4.6-2.9-2 .2-2 .2-2 3.2.2 4.9 3.3 4.9 3.3 2.8 4.9 7.5 3.5 9.3 2.6.3-2.1 1.1-3.5 2-4.3-7-.7-14.5-3.5-14.5-15.7 0-3.5 1.3-6.3 3.3-8.5-.3-.8-1.4-4 .3-8.5 0 0 2.7-.9 8.8 3.3 2.6-.7 5.3-1.1 8-1.1s5.5.3 8 1.1c6.1-4.1 8.7-3.3 8.7-3.3 1.7 4.4.7 7.7.3 8.5 2 2.2 3.3 5.1 3.3 8.5 0 12.2-7.5 15-14.6 15.7 1.1 1 2.2 3 2.2 5.9 0 4.3-.1 7.7-.1 8.7 0 .9.6 1.8 2.2 1.5C54.8 58.8 64 46.8 64 32.7 63.9 15.1 49.6.8 32 .8z"/></svg>
+                    GitHub
+                </a>
             </div>
             <div class="py-2 pr-3">
-                <a href="https://twitter.com/NinanReuben" class="bg-yellow-500 transition duration-200 ease-in-out hover:text-yellow-500 border border-yellow-500 hover:bg-transparent px-4 py-1 shadow-lg rounded-full">Twitter</a>
+                <a href="https://twitter.com/NinanReuben" class="flex items-center bg-yellow-500 transition duration-200 ease-in-out hover:text-yellow-500 border border-yellow-500 hover:bg-transparent px-4 py-1 shadow-lg rounded-full">
+                    <svg class="h-5 w-5 mr-2 my-auto fill-current" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 64 64" xml:space="preserve"><style>.st0{fill:none;stroke:#000;stroke-miterlimit:10}</style><path d="M22.2 58c-5.4 0-13.3-1-20-5.8-2-1.4-2.8-4.1-1.8-6.4.9-2.3 3.4-3.6 5.9-3.1 0 0 1 .2 2.5.1-1-1.4-1.9-3-2.5-4.7-.2-.6-.3-1.1-.3-1.7-2.5-2.9-3.8-6.5-3.8-10.5 0-.9.2-1.8.7-2.5C2 20 1.7 15.5 4.6 10.7 5.5 9.3 7 8.3 8.7 8.1h.2c1.8-.1 3.4.6 4.5 1.8.7.7 6.1 6.4 13 8.3C28 11.2 34.4 6 42.1 6c3.4 0 6.7 1 9.3 2.9.7-.2 1.6-.5 2.4-1.1 1.6-1.1 3.7-1.3 5.4-.4 1.7.8 2.9 2.4 3.1 4.3V12.6c1.4 1.4 1.9 3.4 1.3 5.2-.5 1.7-2.1 3.8-5.4 6.8-.2 7.4-3.7 27.2-28.6 32.8-.2 0-3.2.6-7.4.6zM5.4 47.6c-.2 0-.3.1-.3.1v.1c-.1.1-.1.2 0 .3 5.6 4.1 12.4 4.9 17.1 4.9 3.7 0 6.3-.5 6.4-.5 22.6-5.1 24.8-23.6 24.9-29.2 0-.7.3-1.4.8-1.8 4.2-3.7 4.7-5 4.7-5.1.1-.2-.1-.4-.2-.4l-.1-.1c-1.1-.5-1.7-1.8-1.3-3 .1-.3.1-.5.1-.7 0-.1-.1-.2-.2-.3-.1 0-.1-.1-.2-.1s-.2 0-.4.1c-2.2 1.5-4.5 2-5.6 2.1-.7.1-1.3-.1-1.8-.5-1.9-1.6-4.5-2.4-7.1-2.4-6.1 0-11 4.5-11.2 10.4 0 .7-.3 1.4-.9 1.8-.5.5-1.2.7-1.9.6-9.5-1.3-17-8.9-18.5-10.4l-.1-.1c-.1-.1-.3-.1-.4-.1-.2 0-.3.2-.4.3-1.8 3-2 6.2-.7 9.4.4 1 .2 2.2-.7 2.9l-.2.2c0 3.2 1 5.8 3.2 7.8.7.7.9 1.6.7 2.5v.3c.8 2.5 2.4 4.5 4.6 5.8.8.5 1.3 1.4 1.2 2.4-.1 1-.8 1.8-1.7 2.1-4.6 1.5-9.5.8-9.7.7 0-.1-.1-.1-.1-.1zm53.1-31.9z"/><path class="st0" d="M-5.7.9zM32 32z"/></svg>
+                    Twitter
+                </a>
             </div>
             <div class="py-2">
-                <nuxt-link to="resume" class="bg-yellow-500 transition duration-200 ease-in-out hover:text-yellow-500 border border-yellow-500 hover:bg-transparent px-4 py-1 shadow-lg rounded-full">Resume</nuxt-link>
+                <nuxt-link to="resume" class="flex items-center bg-yellow-500 transition duration-200 ease-in-out hover:text-yellow-500 border border-yellow-500 hover:bg-transparent px-4 py-1 shadow-lg rounded-full">
+                    <svg class="h-5 w-5 mr-2 my-auto fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M55.2 7.5h-1.9V3.7c0-2.1-1.6-3.7-3.7-3.7H9.1C6.9 0 5.3 1.6 5.3 3.7v38.1c0 .8.3 1.3.8 1.9l19.7 19.5c.5.5 1.1.8 1.9.8h21.9c2.1 0 3.7-1.6 3.7-3.7v-3.7l5.1-10.1c.3-.3.3-.8.3-1.1V10.7c0-1.6-1.4-3.2-3.5-3.2zM53.3 16H48v-3.5h5.3V16zM24 53.9l-8.5-8.5H24v8.5zm5.3 4.8V43.2c0-1.9-1.6-3.2-3.2-3.2H10.7V5.3H48v2.1h-1.9c-1.9 0-3.5 1.3-3.5 3.2V45c0 .5 0 .8.3 1.3L48 56.5v2.1H29.3zm21.4-8.8L48 44.5V21.3h5.3v23.2l-2.6 5.4z"/></svg>
+                    Resume
+                </nuxt-link>
             </div>
         </div>
 
