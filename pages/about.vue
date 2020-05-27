1596c767adaede396dbdaa0e64ecc8395e7cf002
Reuben Ninan <intelphysic@gmail.com>
2020-05-27 00:36:17 -0400

    about page, layout draft

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+<section class="text-gray-700 body-font">
+  <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded-full" alt="hero" src="../assets/img/profile.jpg">
+    <div class="text-center lg:w-2/3 w-full">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Reuben Mathew Ninan</h1>
+      <p class="mb-8 leading-relaxed">Reuben Ninan is a 19 year-old Software Engineering (B.Eng) student at York University. </p>
+      <div class="flex justify-center">
+        <nuxt-link to="/" class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">Go Home</nuxt-link>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
